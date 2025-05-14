79111951b2bb74216419c43afec91f8172303169
franck codeur <fleurytaty44@gmail.com>
2025-05-14 13:00:46 +0200

    upgrade-files

--- app.js
+++ app.js
@@ -1,2 +1,27 @@
-const button = document.getElementById("button");
-button.addEventListener("click", showWeather);
+const button = document.querySelector("#button");
+const input = document.querySelector("input");
+const temp = document.querySelector(".temp");
+const cityName = document.querySelector(".city");
+const checkWeather = async () => {
+  const city = input.value;
+  const keyApi = `a342fd9e1325587a773d51fbed9b8260`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?units=metric&q=${city}`;
+
+  const response = await fetch(url + `&appid=${keyApi}`);
+  console.log(response);
+
+  const data = await response.json();
+  console.log(data);
+
+  const correctTemp = Math.floor(data.main.temp) + "°C";
+  temp.textContent = correctTemp;
+  cityName.textContent = data.name;
+};
+
+button.addEventListener("click", () => {
+  if (input.value == "") {
+    alert("Veuillez entré le nom de la ville avant de lancer une recherche");
+  } else {
+    checkWeather();
+  }
+});
